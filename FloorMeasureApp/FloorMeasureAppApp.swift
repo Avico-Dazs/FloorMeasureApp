49cc3b231d43c792560de318bbd984be576b5872
Avico-Dazs <49014711+Avico-Dazs@users.noreply.github.com>
2023-01-06 16:35:24 +0900

    Initial Commit

---
+++ FloorMeasureApp/FloorMeasureAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloorMeasureAppApp.swift
+//  FloorMeasureApp
+//
+//  Created by Yusuke Abiko on 2023/01/06.
+//
+
+import SwiftUI
+
+@main
+struct FloorMeasureAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
